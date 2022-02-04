13f064e7a36ee7fa1275165a1210238d56aa64c7
Seneca <flabbergasted242@gmail.com>
2022-02-04 12:17:33 +0300

    set up navigation bar and button

---
+++ CoreDataWithoutStoryboardTraining/TaskListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CoreDataWithoutStoryboardTraining
+//
+//  Created by Дмитрий Дмитрий on 03.02.2022.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navBarAppearance.backgroundColor =  UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+         )
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        navigationController?.navigationBar.tintColor = .white 
+        
+    }
+    
+    @objc private func addNewTask() {
+        
+    }
+
+
+}
+
