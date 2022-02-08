a15f188ab224999db8a02512861891c7e52caeeb
Seneca <flabbergasted242@gmail.com>
2022-02-08 19:07:29 +0300

    saved data to data base fetched data from data base passed data from TaskVC to TaskListVC

--- CoreDataWithoutStoryboardTraining/TaskViewController.swift
+++ CoreDataWithoutStoryboardTraining/TaskViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskViewController: UIViewController {
+    
+    var delegate: TaskViewControllerDelegate?
    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     private lazy var taskTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "New Task"
@@ -83,10 +88,22 @@ class TaskViewController: UIViewController {
     }
     
     @objc private func save() {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: context) as? Task else { return }
+        task.title = taskTextField.text
+        
+        if context.hasChanges{
+            do {
+                try context.save()
+            } catch let error {
+                print (error.localizedDescription)
+            }
+        }
+        delegate?.reloadData()
         dismiss(animated: true)
     }
     
-    @objc private func cancel() {
+    @objc private func cancel() { 
         dismiss(animated: true)
     }
 }
